93b64c08c282b2be596f3857ee36997a1a0897c3
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-03-05 20:14:53 -0300

    get set

--- Banco/src/main/kotlin/main.kt
+++ Banco/src/main/kotlin/main.kt
@@ -4,7 +4,10 @@ fun main() {
     val contaMaria = Conta()
     contaMaria.titular = "Maria Clara"
     contaMaria.numero = 1000
+    contaMaria.saldo = -200.0
+    println("salto ${contaMaria.saldo}")
     contaWalter.titular = "Walter Heitor"
+    contaWalter.saldo = 205.53
     contaWalter.numero = 1002
 
     println("titular conta : ${contaWalter.titular}")
@@ -12,12 +15,20 @@ fun main() {
     println("Depositando na conta o valor: ${contaWalter.saldo}")
     contaWalter.saca(378.96)
     println("sacando na conta o valor: ${contaWalter.saldo}")
-    print("---------------------------------------------------")
+    println("---------------------------------------------------")
     println("titular conta : ${contaMaria.titular}")
     contaMaria.deposita(7500.0)
     println("Depositando na conta o valor: ${contaMaria.saldo}")
     contaMaria.saca(1257.20)
     println("sacando na conta o valor: ${contaMaria.saldo}")
+    println("---------------------------------------------")
+    println("Tranferencia da conta maria para walter")
+    if(contaMaria.tranferre(297.04, contaWalter)){
+        println("Tranferencia sucedida!")
+        println("conta o valor: ${contaWalter.saldo}")
+    }else{
+        println("Falha na ranferencia!")
+    }
 
 }
 
@@ -28,6 +39,9 @@ class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+    set(valor){
+        if(valor > 0) field = valor
+    }
 
     fun deposita(valor: Double) {
         this.saldo += valor
@@ -35,5 +49,14 @@ class Conta {
 
     fun saca(valor: Double) {
         if (valor <= saldo) this.saldo -= valor
+    }
+
+    fun tranferre(valor: Double, destino: Conta): Boolean {
+        if(saldo >= valor){
+            saldo -=valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
     }
 }
